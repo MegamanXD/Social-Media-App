6bae911e4ef751be92463f20c25cd90a1bbb6f1c
Minh <s3759797@rmit.edu.vn>
2021-01-02 18:12:12 +0700

    Create and Delete posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,20 +1,75 @@
 // 0.1. Declaring all dependency imports
-    // None
+const { AuthenticationError } = require('apollo-server');           // GraphQL built-in error handler
 
 // 0.2. Declaring all file imports
-const Post = require('../../models/Post.js')        // Import Post schema for MongoDB
+const Post = require('../../models/Post.js')
+const checkAuthenticated = require('../../util/check-authenticated.js');
 
 // 1. Details to be exported
 module.exports = {
+    // 1.1. Define all Query (GET methods)
     Query: {
-        // GET method for Posts
+        // Get all posts
         async getPosts(){
             try{
-                const posts = await Post.find();    // Find all Posts from MongoDB
+                let posts = await Post.find()                      // Find all Posts from MongoDB
+                                      .sort({createdAt: -1});;     // Sort to show all newly-created posts at the top
                 return posts;
             } catch (error) {
                 throw new Error(error);
             }
+        },
+
+        // Get 1 specific post by ID
+        async getPost(_, { postId }) {
+            try{
+              const post = await Post.findById(postId);
+
+              if (post) { return post; }
+              else      { throw new Error('Post not found'); }
+            }
+            catch (err){
+              throw new Error(err);
+            }
         }
+    },
+
+    // 1.1. Define all Mutation (non-GET methods)
+    Mutation: {
+      //Create post
+      async createPost(_, { body }, context) {
+        const user = checkAuthenticated(context);
+  
+        if (body.trim() === '') { throw new Error('Post body must not be empty'); }
+  
+        const newPost = new Post({
+          body,
+          user: user.id,
+          username: user.username,
+          createdAt: new Date().toISOString()
+        });
+  
+        const savedPost = await newPost.save();
+  
+        return savedPost;
+      },
+
+      // Delete post
+      async deletePost(_, { postId }, context) {
+        const user = checkAuthenticated(context);
+  
+        try {
+          const post = await Post.findById(postId);
+
+          if (user.username === post.username){
+            await post.delete();
+            return 'Post deleted successfully';
+          }
+          else { throw new AuthenticationError('Action not allowed'); }
+        }
+        catch (err) {
+          throw new Error(err);
+        }
+      }
     }
 }
