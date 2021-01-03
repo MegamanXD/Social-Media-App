6decf6937005c7ea3703f0cd699db3dd3bd4e6b4
Minh <s3759797@rmit.edu.vn>
2021-01-03 16:19:32 +0700

    basic_ui_outline
    Working menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './components/MenuBar.js';
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
 
 function App() {
   return (
-    <div><h1>Hello, world!</h1></div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
